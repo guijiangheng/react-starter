3958d1227c744ce63f2f4dcc276ee57d115e526b
guijiangheng <kakaxizhh@gmail.com>
2021-10-10 22:45:26 +0800

    setup alias for @

---
+++ README.md
@@ -0,0 +1,10 @@
+# React 模板项目
+
+工具箱
+
+- react + typescript
+- vite
+- eslint + stylelint
+- prettier
+- tailwind
+- husky + lint-staged
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from '@/components/App';
 
 ReactDOM.render(
   <React.StrictMode>
