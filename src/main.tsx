96d967293c5da1b047c97fc65816ed8364aed0fb
guijiangheng <kakaxizhh@gmail.com>
2021-10-17 20:54:00 +0800

    feat: add page links

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Dashboard } from '@/components/Dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dashboard />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
